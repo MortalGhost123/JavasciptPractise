242a1697fffb135a1ce0156ce1d6e2718c7965aa
Kumar Swapnil <kumarswapnil154@gmail.com>
2025-07-31 12:28:44 +0530

    Add conversion of data types

---
+++ 03_conversionOperations.js
@@ -0,0 +1,31 @@
+let score = false;
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
+/*
+    "33" => 33
+    "33abds" => NaN
+    true => 1 ; false => 0
+*/
+
+let isThere = undefined;
+
+let booleanIsThere = Boolean(isThere);
+
+// console.log(booleanIsThere);
+
+/*
+    1 => true ; 0 => false
+    "" => false
+    "swapnil" => true
+*/
+
+let no = 33
+let stringNumeber = String(no);
+console.log(stringNumeber)
+console.log(typeof stringNumeber)
