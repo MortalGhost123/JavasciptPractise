e1d59dde77315ff469bdca4f79f72e653b4ed1ce
Kumar Swapnil <kumarswapnil154@gmail.com>
2025-07-31 14:13:17 +0530

    Add conversion operations

--- 03_conversionOperations.js
+++ 03_conversionOperations.js
@@ -1,3 +1,4 @@
+"use strict"; 
 let score = false;
 
 // console.log(typeof score)
@@ -27,5 +28,47 @@ let booleanIsThere = Boolean(isThere);
 
 let no = 33
 let stringNumeber = String(no);
-console.log(stringNumeber)
-console.log(typeof stringNumeber)
+// console.log(stringNumeber)
+// console.log(typeof stringNumeber)
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**4)
+
+let str1 = "hello"
+let str2 = " world"
+
+let str3 =  str1 + str2
+// console.log(str3)
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log(1+3);
+// console.log(1+2+"3");
+// console.log("1"+2+5);
+
+// console.log(+true);
+// console.log(+"");
+// The unary plus (+) tries to convert its operand into a number.
+
+let gameCounter = 1;
+gameCounter++;
+// console.log(gameCounter);
+
+
+let x2 = 3n;
+const y2 = x2++;
+console.log("x2:", x2);      // Should show 4n
+console.log("y2:", y2);      // Should show 3n
+console.log(typeof x2);      // 'bigint'
+console.log(typeof y2);      // 'bigint'
+
+
+
+
