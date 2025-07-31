d3868ee6ff841728e48c1773a8c9f223467beff0
Kumar Swapnil <kumarswapnil154@gmail.com>
2025-07-31 10:06:46 +0530

    Add variable use

---
+++ variables.js
@@ -0,0 +1,18 @@
+console.log("hello")
+const accountId = 12;
+var accountName = "Harilal";
+let accountAddress = "Pune";
+accountBalance = 123.44;
+let accountState;
+
+/*
+    var is not preferable to use because of issue of block scope and functional scope
+*/
+
+// accountId = 13; not allowed
+accountName = "Mohan"
+accountAdress = "Jaipur"
+accountBalance = 12222.3332222223211111111111111111111111111111111111;
+
+console.table([accountId,accountName,accountAdress,accountBalance,accountState]);
+
